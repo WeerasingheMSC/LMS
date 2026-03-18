83a9605b7ae42edf6797a1774a6bd4f8ba03d976
WeerasingheMSC <sahanchampathi2001ugc@gmail.com>
2026-03-18 10:48:51 +0530

    Feat(Frontend): Generated vite project for frontend with tailwindcss

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-600 text-4xl justify-center text-center mt-10'>Welcome to the Frontend!</h1>
+      
+    </>
+  )
+}
+
+export default App
